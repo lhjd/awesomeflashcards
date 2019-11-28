414d160a88017725b5e7300a7f6084ea81c8a4e8
lhjd <hongjinlau@gmail.com>
2019-11-28 14:54:06 +0800

    Format the card

--- app/javascript/packs/components/Card.jsx
+++ app/javascript/packs/components/Card.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
+    width: 100,
+    height: 100,
   },
 }));
 
@@ -13,13 +16,21 @@ export default function PaperSheet() {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        This is a sheet of paper.
-      </Typography>
-      <Typography component="p">
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center">
+      <Paper className={classes.root}>
+        <Typography 
+          variant="h5"
+          component="h3">
+          Wow
+        </Typography>
+        {/* <Typography component="p">
         Paper can be used to build surface or other elements for your application.
-      </Typography>
-    </Paper>
+      </Typography> */}
+      </Paper>
+    </Box>
   );
 }
