cbf84e3ac26798fbbbd2f9f60b6e5f0e96a19a26
lhjd <hongjinlau@gmail.com>
2019-11-29 14:53:12 +0800

    Add flip css

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 import Controls from './components/Controls';
+import Box from '@material-ui/core/Box';
 
 export const Context = React.createContext(null);
 
@@ -10,8 +11,9 @@ const reducer = (state, action) => {
     case 'NEXT_WORD':
       if (state.wordIndex < state.words.length - 1) {
           let newWordIndex = state.wordIndex + 1;
-          let newWord = state.words[newWordIndex];
-          return {...state, wordIndex: newWordIndex, word: newWord };
+          let newFrontWord = state.words[newWordIndex].front;
+          let newBackWord = state.words[newWordIndex].back;
+          return {...state, wordIndex: newWordIndex, frontWord: newFrontWord, backWord: newBackWord };
       } else {
           return state;
       }
@@ -20,8 +22,9 @@ const reducer = (state, action) => {
           return state;
       } else {
           let newWordIndex = state.wordIndex - 1;
-          let newWord = state.words[newWordIndex];
-          return {...state, wordIndex: newWordIndex, word: newWord };
+          let newFrontWord = state.words[newWordIndex].front;
+          let newBackWord = state.words[newWordIndex].back;
+          return {...state, wordIndex: newWordIndex, frontWord: newFrontWord, backWord: newBackWord };
       }
     default:
       throw new Error("reducer error");
@@ -30,17 +33,26 @@ const reducer = (state, action) => {
 
 export default function App() {
 
-    const words = ["a, an", "about", "above", "across","after", "again"];
-    let word = words[0];
+    const words = [{front: "a, an", back: "apple"}, 
+                    {front: "about", back: "banana"},
+                    {front: "above", back: "cranberry"},
+                    {front: "across", back: "durian"}, 
+                    {front: "after", back: "egg"}, 
+                    {front: "again", back: "fruit"}];
 
-    const [state, dispatch] = useReducer(reducer, { wordIndex: 0, words: words, word: word });
+    let frontWord = words[0].front;
+    let backWord = words[0].back;
+
+    const [state, dispatch] = useReducer(reducer, { wordIndex: 0, words, frontWord, backWord });
     
     return (
         <>
             <Navbar />
             <Context.Provider value={dispatch}>
-                <Card word={state.word} />
-                {/* <Controls /> */}
+                <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                    <Card frontWord={state.frontWord} backWord={state.backWord} />
+                    <Controls />
+                </Box>
             </Context.Provider>
         </>
     );
--- app/javascript/packs/components/Card.jsx
+++ app/javascript/packs/components/Card.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import styles from './Card.module.scss';
+import classnames from 'classnames';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -12,24 +14,47 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const cx = classnames.bind(styles)
+
 export default function PaperSheet(props) {
+
+  let {frontWord, backWord} = props;
 
   const classes = useStyles();
 
+  const [flipped, setFlipped] = useState(false);
+
+  const flipCardInner = cx(
+    styles.flipCardInner, // styles that never change
+    { // dynamic styles
+      [styles.flipped]: flipped // make the key the style name, and the value the dynamic boolean
+    }
+  )
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  }
+
   return (
-    <Box
-      // display="inline-block"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center">
-      <Paper className={classes.root} elevation={24}>
-        <Typography 
-          variant="h5"
-          component="h3">
-          {props.word}
-        </Typography>
-      </Paper>
+
+
+    <Box className={styles.flipCard} onClick={handleClick}>
+      <Box className={flipCardInner}>
+        <Box className={styles.flipCardFront}>
+          <Paper className={classes.root} elevation={24}>
+            <Typography variant="h5" component="h3">
+              {frontWord}
+            </Typography>
+          </Paper>
+        </Box>
+        <Box className={styles.flipCardBack}>
+          <Paper className={classes.root} elevation={24}>
+            <Typography variant="h5" component="h3">
+              {backWord}
+            </Typography>
+          </Paper>          
+        </Box>
+      </Box>
     </Box>
   );
 }
