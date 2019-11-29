4d6b1d79142b2c3d03dceca9fd3807c86602d589
lhjd <hongjinlau@gmail.com>
2019-11-29 16:12:41 +0800

    Try to deploy css module

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,19 @@
-// const { environment } = require('@rails/webpacker')
-
-// module.exports = environment
-
 const { environment } = require('@rails/webpacker')
-const merge = require('webpack-merge')
-
-const myCssLoaderOptions = {
-  modules: {
-    localIdentName: '[name]__[local]___[hash:base64:5]'
-  },
-  sourceMap: true,
-}
-
-const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
-
-CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
 
 module.exports = environment
+
+// const { environment } = require('@rails/webpacker')
+// const merge = require('webpack-merge')
+
+// const myCssLoaderOptions = {
+//   modules: {
+//     localIdentName: '[name]__[local]___[hash:base64:5]'
+//   },
+//   sourceMap: true,
+// }
+
+// const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+
+// CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
+
+// module.exports = environment
