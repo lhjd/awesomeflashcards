1c6c1ec09f57c70b7bd86f734532f0a520e54872
lhjd <hongjinlau@gmail.com>
2019-12-02 09:05:39 +0800

    Add continue button

---
+++ app/javascript/packs/components/ContinueButton.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { Context } from '../App';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ContainedButtons() {
+  const classes = useStyles();
+
+
+  const dispatch = useContext(Context);
+
+  const handleClick = () => {
+        dispatch({ type: "NEXT_WORD" });
+}
+  
+
+  return (
+    <div className={classes.root}>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Continue
+      </Button>
+    </div>
+  );
+}
--- app/javascript/packs/components/Controls.jsx
+++ app/javascript/packs/components/Controls.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
 import Button from './Button';
 import Box from '@material-ui/core/Box';
+import ContinueButton from './ContinueButton';
 
 export default function Controls() {
 
     return (
         <>
             <Box display="flex" flexDirection="row">
-                <Button actionType="PREVIOUS_WORD" btnText="arrow_back_ios" />
-                <Button actionType="NEXT_WORD" btnText="arrow_forward_ios" />
+                {/* <Button actionType="PREVIOUS_WORD" btnText="arrow_back_ios" />
+                <Button actionType="NEXT_WORD" btnText="arrow_forward_ios" /> */}
+                <ContinueButton />
             </Box>
         </>
     );
